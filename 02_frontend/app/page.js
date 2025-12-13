4c450aeff881f9ef0d112e2c2ca10ac5d41900bb
sangraces <sangrace2548@gmail.com>
2025-12-13 15:15:53 +0700

    Add my name

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,6 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
+        <h1 className="title">Rasita Srivilaived 6603105</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
